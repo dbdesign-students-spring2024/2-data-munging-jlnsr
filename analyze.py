e2b51aa3c520b7505809464c80e35b1da80d5128
jlnsr <julien.sotoraspa@gmail.com>
2024-02-12 23:25:31 -0500

    done!

--- analyze.py
+++ analyze.py
@@ -1,1 +1,40 @@
 # Place code below to do the analysis part of the assignment.
+with open("clean_data.csv","r") as file:
+    full_data = file.readlines()
+    full_data = [i.split("\t") for i in full_data]
+    for i in full_data: #remove the first line 
+        for j in i:
+            if str.lower(j) == 'year':
+                full_data.remove(i)
+    full_data = [i[:13] for i in full_data] #extract just the relevant values (Jan - Dec)
+    full_data.pop()
+    records_as_dict = {i[0]:i[1:] for i in full_data}
+
+    for record in records_as_dict.values():
+        for index,value in enumerate(record):
+            if value != "\n":
+                try:
+                    record[index] = float( record[index] )
+                except:
+                    continue
+            else:
+                record.remove(value)
+    total = 0   #total count for all months in each year of EACH decade
+                #starting at 1880
+    counter = 0 #keep track of how many years, stop at 9
+    
+    averages_list = [] #list holding JUST averages
+    decades_list = [] #list of decades
+    for key,record in records_as_dict.items():
+        if counter == 0:
+            decades_list += [ key+ "-" + str( int(key)+9 )]    #create a key for the beginning year of each decade e.g. 1890,1900,etc. that will hold the average value
+        if counter < 9:
+            total += sum(record)
+            counter += 1
+        else:
+            averages_list += [format(total/120, '.2f')] #total / amount of months in a decade = average
+            counter = 0 #rest variables
+            total = 0 
+        
+    for decade,average in zip(decades_list, averages_list): #output the averages according to decade
+        print(decade,": ",average,sep="")
--- munge.py
+++ munge.py
@@ -1,1 +1,58 @@
-# Place code below to do the munging part of this assignment.
+""" Workspace Module."""
+#/NYU DOCUMENTS/2-data-munging-jlnsr/data/nasa.txt
+
+#J-D average for entire year (Jan-December)
+#D-N average between Dec of PREVIOUS year to current Nov
+#DJF average of Dec of previous year through Feb of current year
+#MAM average of Mar-May
+#JJA average of Jun-Aug
+#SON average of Sep-Nov
+with open("data/nasa.txt","r") as file:
+    all_data = file.readlines() #a list containing each line of the file as a string
+    raw_data = []
+    start_read = ''
+
+    for index,line in enumerate(all_data): #the first lines up to the first lines of data
+        if (str.lower(line)).startswith('year'):
+            all_data = all_data[index+1:]
+            break
+    all_data.reverse() #temporarily reverse the data set to remove excess lines from the bottom up 
+    for index,line in enumerate(all_data): #iterate through the list backwards and remove excess lines at the bottom
+        if (str.lower(line)).startswith('year'):
+            all_data = all_data[index+1:]
+            break       
+    all_data.reverse()
+    
+    all_data = [line.strip() for line in all_data] #remove \n
+    all_data = [line[:-4] for line in all_data if line.endswith( line[0:4])] #removing any reoccurences of the column/row fields
+        
+    records_as_dict = {} #a dictionary where every 'key' is the year and every 'value' is a list of temps of that year
+    values = [] #a temporary list that will acumulate all the acceptable values in each line of data in 'cleaned_data'
+    for line in all_data: 
+        for i in line.split(" "): #iterate through each line as a list
+            if (i != "") and (i != line[:4]): #remove excess spaces AND remove the year to isolate just the temperature values
+                values += [i]
+        records_as_dict[line[:4]] = values
+        values = [] #clear the list for new values
+    
+    for key in records_as_dict.copy(): #remove anything other than a year
+        if key.isnumeric() is False:
+            records_as_dict.pop(key)
+
+    records_as_dict_converted = {key:[] for key in records_as_dict.keys()} #dictionary holding the converted values
+
+    for key,record in records_as_dict.items():
+        for value in record:
+            try:
+                records_as_dict_converted[key] += [format((( int(value)/100 ) * 1.8),".1f" )] #convert from celsius to Fahrenheit, and FORMAT result
+            except:
+                records_as_dict_converted[key] += [value]
+
+line = '' #a variable to write every line for the new CSV file
+
+with open("clean_data.csv","w") as file2:
+    for key,record in records_as_dict_converted.items():
+        for value in record:
+            line += value+"\t"
+        file2.write(key+"\t"+line+"\n")
+        line = ""
